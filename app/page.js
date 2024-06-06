a35d95109b9b7ac2a88bf67db00b2db3d88ef098
Waris Ansar <bhehzadprince@gmail.com>
2024-06-07 02:06:54 +0500

    footer responsive

--- app/page.js
+++ app/page.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="heading1 text-secondaryGreen">
-      This is the main home page
+    <div className="max-container heading3 text-secondaryGreen">
+      Hero sections
     </div>
   );
 }
