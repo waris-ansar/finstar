40295a89f1f38f865634486487b72f1a54d0189d
Waris Ansar <bhehzadprince@gmail.com>
2025-06-18 23:55:43 +0500

    check commit

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,9 +1,10 @@
-import Content from '../contents/about.mdx';
+import Content from "../contents/about.mdx";
 import MdWrapper from "../../components/MdWrapper";
 import Advantages from "../../components/Advantages";
 import Banner from "../../components/Banner";
 
 const Page = () => (
+  // checking commit
   <>
     <MdWrapper title="About Us">
       <Content />
